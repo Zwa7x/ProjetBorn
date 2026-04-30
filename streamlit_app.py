4d4590b9150ed4d0cbf4019ba86b6e59f1015329
Zwa7x <florian.braconnier@gmail.com>
2026-04-30 15:15:37 +0200

    Update title for electric car consumption tracking

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title("🚗 Suivi de consommation voiture électrique")
+st.title("Suivi consommation CUPRA Born")
 
 df = pd.read_excel("CONSO_CUPRA.xlsx")
 st.dataframe(df)

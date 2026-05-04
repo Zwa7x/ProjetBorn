9355b3f942fc519f844d47f8200fd7453a5a314d
Zwa7x <florian.braconnier@gmail.com>
2026-05-04 10:42:02 +0200

    Replace kWh input with power input in form

--- pages/2_📝_Saisie.py
+++ pages/2_📝_Saisie.py
@@ -9,7 +9,7 @@ df = load_data()
 with st.form("add_form"):
     date = st.date_input("Date")
     station = st.text_input("Station")
-    kwh = st.number_input("kWh chargés", min_value=0.0)
+    puissance = st.number_input("Puissance délivrée (kW)", ...)
     cout = st.number_input("Coût (€)", min_value=0.0)
     submit = st.form_submit_button("Ajouter")
 
@@ -17,7 +17,7 @@ if submit:
     new_row = pd.DataFrame([{
         "Date": date,
         "Station": station,
-        "kWh": kwh,
+        "Puissance": Puissance,
         "Cout": cout
     }])
     df = pd.concat([df, new_row], ignore_index=True)

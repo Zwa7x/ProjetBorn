714530545c4de23765eb17174fa0e15f60851978
Zwa7x <florian.braconnier@gmail.com>
2026-05-06 14:47:37 +0200

    Delete utils/__inti__.py

--- utils/__inti__.py
+++
@@ -1,5 +0,0 @@
-# utils/__init__.py
-from .helpers import load_data, save_data
-from .settings_loader import load_settings, save_settings
-
-__all__ = ["load_data", "save_data", "load_settings", "save_settings"]

2f06f63f2f86fcdb597b8a4cf87ac99ea12c2e48
Zwa7x <florian.braconnier@gmail.com>
2026-05-12 11:13:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ df = load_table("mesures")            # charge la table
 save_result = save_table_upsert("mesures", edited_df, mode="upsert")
 st.write(save_result)                 # affiche {"inserted": X, "skipped": Y}
 
-ingest_excel("data/source.xlsx", mode="upsert")
+ingest_excel("data/CONSO_CUPRA.xlsx", mode="upsert")
 # ou pour une table spécifique après modification en mémoire
 save_table_upsert("regions", df_regions, mode="upsert")
 
